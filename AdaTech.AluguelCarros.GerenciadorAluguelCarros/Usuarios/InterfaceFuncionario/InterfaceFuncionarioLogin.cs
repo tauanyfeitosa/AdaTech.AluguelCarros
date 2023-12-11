d9dc9d24519056a07396d2f3ff13bc9455162072
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-12-11 10:31:35 -0300

    create: classes para o gerenciamento da interação com o funcionário

---
+++ AdaTech.AluguelCarros.GerenciadorAluguelCarros/Usuarios/InterfaceFuncionario/InterfaceFuncionarioLogin.cs
@@ -0,0 +1,60 @@
+﻿
+namespace AdaTech.AluguelCarros.GerenciadorAluguelCarros.Usuarios.InterfaceFuncionario
+{
+    internal class InterfaceFuncionarioLogin
+    {
+        internal static Funcionario AdicionarFuncionario()
+        {
+            Console.WriteLine("Informe o CPF do funcionário:");
+            var cpfFuncionario = Console.ReadLine().Replace(".", "").Replace("-", "");
+
+            var funcionarioExistente = AcervoFuncionario.SelecionarFuncionario(cpfFuncionario);
+
+            if (funcionarioExistente != null)
+            {
+                Console.WriteLine("Funcionário já existe na lista.");
+                return funcionarioExistente;
+            }
+
+            Console.WriteLine("O funcionário não foi encontrado em nosso sistema, faremos o " +
+                "cadastro do mesmo. Informe os dados abaixo:");
+
+            var nomeFuncionario = ObterNomeFuncionario();
+            var senha = ObterSenha();
+            var confirmacaoSenha = ConfirmarSenha();
+
+            try
+            {
+                var novoFuncionario = new Funcionario(nomeFuncionario, cpfFuncionario, senha, confirmacaoSenha);
+
+                AcervoFuncionario.AdicionarFuncionario(novoFuncionario);
+                Console.WriteLine("Funcionário adicionado com sucesso!");
+                return novoFuncionario;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+        }
+
+        private static string ObterNomeFuncionario()
+        {
+            Console.WriteLine("Informe o nome do funcionário:");
+            return Console.ReadLine();
+        }
+
+        private static string ObterSenha()
+        {
+            Console.WriteLine("Informe a senha do usuário:");
+            return Console.ReadLine();
+        }
+
+        private static string ConfirmarSenha()
+        {
+            Console.WriteLine("Confirme a sua senha:");
+            return Console.ReadLine();
+        }
+
+    }
+}
---
+++ AdaTech.AluguelCarros.GerenciadorAluguelCarros/Usuarios/InterfaceFuncionario/MenuFuncionario.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+
+namespace AdaTech.AluguelCarros.GerenciadorAluguelCarros.Usuarios.InterfaceFuncionario
+{
+    internal class MenuFuncionario
+    {
+        internal static void ExibirMenu(Funcionario funcionario)
+        {
+            bool sair = false;
+
+            do
+            {
+                Console.Clear(); 
+
+                Console.WriteLine($"Bem-vindo, {funcionario.Nome}!");
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1. Visualizar informações pessoais");
+                Console.WriteLine("2. Realizar alguma tarefa específica");
+                Console.WriteLine("3. Sair");
+
+                if (int.TryParse(Console.ReadLine(), out int escolha))
+                {
+                    switch (escolha)
+                    {
+                        case 1:
+                            VisualizarInformacoesPessoais(funcionario);
+                            break;
+                        case 2:
+                            RealizarTarefaEspecifica(funcionario);
+                            break;
+                        case 3:
+                            sair = true;
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida. Tente novamente.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Tente novamente.");
+                }
+
+                Console.WriteLine("Pressione Enter para continuar...");
+                Console.ReadLine();
+
+            } while (!sair);
+        }
+
+        private static void VisualizarInformacoesPessoais(Funcionario funcionario)
+        {
+            Console.WriteLine($"Nome: {funcionario.Nome}");
+            Console.WriteLine($"CPF: {funcionario.LoginCPF}");
+        }
+
+        private static void RealizarTarefaEspecifica(Funcionario funcionario)
+        {
+            Console.WriteLine("Executando tarefa específica para o funcionário...");
+        }
+    }
+}
+
