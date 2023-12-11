839bb73f99dba08dd2746374b40bce54e7101b53
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-12-10 21:04:18 -0300

    create: classe para contingência de todas reservas criadas

---
+++ AdaTech.AluguelCarros.GerenciadorAluguelCarros/GerenciamentoInterno/EstoqueReservas.cs
@@ -0,0 +1,25 @@
+﻿
+namespace AdaTech.AluguelCarros.GerenciadorAluguelCarros.GerenciamentoInterno
+{
+    internal class EstoqueReservas
+    {
+        private static List <Reserva> _listaReservas = new List <Reserva>();
+
+        internal static void AdicionarReserva(Reserva reserva)
+        {
+            _listaReservas.Add(reserva);
+        }
+
+        internal static void ExcluirReserva(int id)
+        {
+            _listaReservas.RemoveAll(reserva => reserva.Id == id);
+        }
+
+        internal static Reserva SelecionarReserva(int id)
+        {
+            return _listaReservas.FirstOrDefault(reserva => reserva.Id == id);
+        }
+
+
+    }
+}
