30e7ff9e200c8243d51d5b07767c7d43e37e87f1
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-12-10 21:03:27 -0300

    fix: alteração do nome da classe para melhor entendimento e modificação dos métodos e atributos para estáticos

---
+++ AdaTech.AluguelCarros.GerenciadorAluguelCarros/Veiculos/EstoqueVeiculos.cs
@@ -0,0 +1,25 @@
+﻿
+namespace AdaTech.AluguelCarros.GerenciadorAluguelCarros.Veiculos
+{
+    internal class EstoqueVeiculos
+    {
+        private static List<Veiculo> _listaVeiculos = new List<Veiculo> ();
+
+        internal static void AdicionarVeiculo(int assentos, int portas, decimal precoDiria, string modelo, string placa,
+    TipoVeiculos veiculos)
+        {
+            var veiculo = new Veiculo(_listaVeiculos.Count, assentos, portas, precoDiria, modelo, placa, veiculos);
+            _listaVeiculos.Add(veiculo);
+        }
+
+        internal static void ExcluirVeiculo(int id)
+        {
+            _listaVeiculos.RemoveAll(veiculo => veiculo.Id == id);
+        }
+
+        internal static Veiculo SelecionarVeiculo (int id)
+        {
+            return _listaVeiculos.FirstOrDefault(veiculo => veiculo.Id == id);
+        }
+    }
+}
