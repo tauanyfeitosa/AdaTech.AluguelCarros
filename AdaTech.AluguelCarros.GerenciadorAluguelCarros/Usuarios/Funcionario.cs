20fa5004851f0a5406a4cc8b091d9c156d53221c
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-12-10 23:29:18 -0300

    create: classe funcionario para gerenciar as demandas internas da empresa

---
+++ AdaTech.AluguelCarros.GerenciadorAluguelCarros/Usuarios/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿
+namespace AdaTech.AluguelCarros.GerenciadorAluguelCarros.Usuarios
+{
+    using Veiculos;
+    internal class Funcionario
+    {
+        private readonly string? _nome;
+        private readonly int _id;
+        private readonly string? _loginCPF;
+        private string? _senha;
+        private bool _ativo;
+
+        public Funcionario(string nome, string loginCPF, string senha, string confirmacaoSenha)
+        {
+            if (senha != confirmacaoSenha)
+            {
+                throw new ArgumentException("As senhas não correspondem!");
+            }
+
+            this._nome = nome;
+            this._loginCPF = loginCPF;
+            this._senha = senha;
+        }
+
+        internal void CadastrarVeiculo(int assentos, int portas, decimal precoDiria, string modelo, string placa,
+            TipoVeiculos veiculos)
+        {
+            EstoqueVeiculos.AdicionarVeiculo(assentos, portas, precoDiria, modelo, placa, veiculos);
+
+        }
+    }
+}
